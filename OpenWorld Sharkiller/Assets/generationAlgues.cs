c29a3cea7a30d7234e207bb87cc8d827ee225091
l.hoarau <l.hoarau@rubika-edu.re>
2026-09-25 12:05:03 +0400

    Biome , texture et alea object

--- OpenWorld Sharkiller/Assets/generationAlgues.cs
+++ OpenWorld Sharkiller/Assets/generationAlgues.cs
@@ -3,23 +3,22 @@ using UnityEngine;
 public class generationAlgues : MonoBehaviour
 {
     public GameObject Algues;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        for (int i = 0; i < 75 * 2; i++)
+        // Se limite au premier quadrant (i < 75 et j < 75)
+        for (int i = 0; i < 75; i++)
         {
-            for (int j = 0; j < 75 * 2; j++)
+            for (int j = 0; j < 75; j++)
             {
-                int rng = Random.Range(0, 250);
+                int rng = Random.Range(0, 50);
                 if (rng == 0)
                 {
-                    Instantiate(Algues, new Vector3(i, 1, j), Quaternion.identity);
-
+                    // Multiplié par 4 pour correspondre à l'espacement des cubes
+                    Vector3 position = new Vector3((i * 4) + 2, 1, (j * 4) + 2);
+                    Instantiate(Algues, position, Quaternion.identity);
                 }
             }
-
         }
-
-        
     }
 }
